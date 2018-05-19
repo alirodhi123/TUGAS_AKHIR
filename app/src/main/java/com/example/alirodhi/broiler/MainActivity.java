c0d417ad4155a724243d7cf8f512165f552d4aee
DESKTOP-148HI6V\alirodhi <alirodhi123@gmail.com>
2018-05-20 01:55:54 +0700

    changed remote activity

--- app/src/main/java/com/example/alirodhi/broiler/MainActivity.java
+++ app/src/main/java/com/example/alirodhi/broiler/MainActivity.java
@@ -9,6 +9,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.alirodhi.broiler.adapter.ViewPageAdapter;
 import com.example.alirodhi.broiler.fragment.HomeFragment;
--- app/src/main/java/com/example/alirodhi/broiler/fragment/RemoteFragment.java
+++ app/src/main/java/com/example/alirodhi/broiler/fragment/RemoteFragment.java
@@ -7,10 +7,15 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.Toast;
+import android.widget.ToggleButton;
 
 import com.example.alirodhi.broiler.R;
 
 public class RemoteFragment extends Fragment {
+
     public RemoteFragment() {
         // Required empty public constructor
     }
@@ -19,6 +24,56 @@ public class RemoteFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_status, container, false);
+        View view = inflater.inflate(R.layout.fragment_remote, container, false);
+
+        ToggleButton toggleButtonFan = (ToggleButton)view.findViewById(R.id.toggleBtnFan);
+        toggleButtonFan.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked){
+                    Toast.makeText(getActivity(), "Toggle button spray is on", Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(getActivity(), "Toggle button spray is off", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
+        ToggleButton toggleButtonLamp = (ToggleButton)view.findViewById(R.id.toggleBtnLamp);
+        toggleButtonLamp.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked){
+                    Toast.makeText(getActivity(), "Toggle button Lamp is on", Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(getActivity(), "Toggle button Lamp is off", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
+        ToggleButton toggleButtonSpray = (ToggleButton)view.findViewById(R.id.toggleBtnSpray);
+        toggleButtonSpray.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked){
+                    Toast.makeText(getActivity(), "Toggle button spray is on", Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(getActivity(), "Toggle button spray is off", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
+        ToggleButton toggleButtonFanOut = (ToggleButton)view.findViewById(R.id.toggleBtnFanOut);
+        toggleButtonFanOut.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked){
+                    Toast.makeText(getActivity(), "Toggle button fan out is on", Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(getActivity(), "Toggle button fan out is off", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
+        return view;
     }
 }
